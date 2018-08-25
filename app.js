73a195458b77b76fdfd27187cf1dce53076ebbdd
Raul Castellanos <raulcastellanosh@hotmail.com>
2018-08-25 16:48:23 -0500

    Logos y pequeñas cosas

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var express = require('express'),
 	app		= express();
 
+const mongoose = require('mongoose');
+const path = require('path');
+const passport = require('passport');
+const flash = require('connect-flash');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+//Conexion
+//mongoose.connect('mongodb://localhost/3000',{
+
+
+//})
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+
+
+
 
 // Port opening
 app.listen(process.env.PORT || 3000, function(){
