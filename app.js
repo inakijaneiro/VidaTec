02af7f478d0e985b00a301dc0d1ef5b93ee681fd
Iñaki Janeiro Olague <inaki_janeiro@live.com.mx>
2018-08-25 19:37:43 -0500

    Fixed dependencies

--- app.js
+++ app.js
@@ -8,22 +8,6 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 
-
-const mongoose = require('mongoose');
-const path = require('path');
-const passport = require('passport');
-const flash = require('connect-flash');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-
-//Conexion
-//mongoose.connect('mongodb://localhost/3000',{
-
-
-//})
-
 app.get('/', function (req, res) {
   res.sendFile(__dirname+'/html/page.html');
 });
