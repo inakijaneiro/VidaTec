be2cedfeffd667e82fc22453ecdbc56309fc68ef
Sergio Diosdado <sgdc_98@hotmail.com>
2018-08-25 23:17:53 -0500

    updated

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+let password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password"),
+  button = document.getElementById("disabled");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    confirm_password.style.border = "1px solid red";
+  } else {
+    confirm_password.setCustomValidity('');
+    button.disabled = false;
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
