8e4b4ef9ffb07aab0d8001de1b755a1ec13de2a0
Iñaki Janeiro Olague <inaki_janeiro@live.com.mx>
2018-08-26 02:23:48 -0500

    DB integration with system and rout setup

---
+++ public/scripts/script.js
@@ -0,0 +1,22 @@
+let password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password"),
+  button = document.getElementById("disabled");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    confirm_password.style.border = "1px solid red";
+  } else {
+    confirm_password.setCustomValidity('');
+    button.disabled = false;
+  }
+}
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+function openNav() {
+  document.getElementById("myMenu").style.transform = "translateX(70%)";
+}
+function closeNav() {
+  document.getElementById("myMenu").style.transform = "translateX(170%)";
+}
